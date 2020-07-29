4b3f55bf097f6a5a87fa0364c30503f27225cd4d
kenkyu392 <57144941+kenkyu392@users.noreply.github.com>
2020-07-29 16:58:48 +0900

    Use http.CanonicalHeaderKey

--- header.go
+++ header.go
@@ -37,13 +37,14 @@ func ResponseHeader(fs ...HeaderFunc) func(http.Handler) http.Handler {
 // AllowHTTPHeader is middleware that allows a request only when one
 // of the specified strings is included in the specified request header.
 func AllowHTTPHeader(badStatus int, name string, values ...string) func(http.Handler) http.Handler {
+	key := http.CanonicalHeaderKey(name)
 	list := make([]string, len(values))
 	for i, v := range values {
 		list[i] = strings.ToLower(v)
 	}
 	return func(next http.Handler) http.Handler {
 		fn := func(w http.ResponseWriter, r *http.Request) {
-			value := strings.ToLower(r.Header.Get(name))
+			value := strings.ToLower(r.Header.Get(key))
 			for _, v := range list {
 				if strings.Contains(value, v) {
 					next.ServeHTTP(w, r)
@@ -59,13 +60,14 @@ func AllowHTTPHeader(badStatus int, name string, values ...string) func(http.Han
 // DisallowHTTPHeader is middleware that disallows a request only when one
 // of the specified strings is included in the specified request header.
 func DisallowHTTPHeader(badStatus int, name string, values ...string) func(http.Handler) http.Handler {
+	key := http.CanonicalHeaderKey(name)
 	list := make([]string, len(values))
 	for i, v := range values {
 		list[i] = strings.ToLower(v)
 	}
 	return func(next http.Handler) http.Handler {
 		fn := func(w http.ResponseWriter, r *http.Request) {
-			value := strings.ToLower(r.Header.Get(name))
+			value := strings.ToLower(r.Header.Get(key))
 			for _, v := range list {
 				if strings.Contains(value, v) {
 					w.WriteHeader(badStatus)
