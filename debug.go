8655668770c6d36ae990cb8e9391d341c003081b
kenkyu392 <57144941+kenkyu392@users.noreply.github.com>
2021-05-05 21:22:12 +0900

    Change the internal implementation of the Debug middleware to Switch

--- debug.go
+++ debug.go
@@ -4,17 +4,10 @@ import (
 	"net/http"
 )
 
-// Debug provides middleware that executes the handler only if d is true.
-// If d is false, it will return 404.
-func Debug(d bool) func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		fn := func(w http.ResponseWriter, r *http.Request) {
-			if d {
-				next.ServeHTTP(w, r)
-				return
-			}
-			http.NotFound(w, r)
-		}
-		return http.HandlerFunc(fn)
-	}
+// Debug provides middleware that executes the handler only if b is true.
+// If b is false, it will return 404.
+func Debug(b bool) func(http.Handler) http.Handler {
+	return Switch(func(r *http.Request) bool {
+		return b
+	}, http.NotFoundHandler())
 }
